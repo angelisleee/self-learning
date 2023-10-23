90384002bb005e41a6341fc144e9674273d062a7
angelisleee <145657907+angelisleee@users.noreply.github.com>
2023-10-23 08:49:17 +0200

    Create DayofWeek.java

---
+++ DayofWeek.java
@@ -0,0 +1,12 @@
+public class DayofWeek{
+  public static void main(String[] args){
+      int dayOfWeek = Integer.parseInt(args[0]);
+
+      switch(dayOfWeek){
+          case 0 :
+          System.out.println("Montag");
+          case 1 :
+          System.out.println("Dienstag");
+      }
+  }
+}
